45dd5329efde39e223f98150b46c430c800323b3
dsthiago <thiagosd.2.18@gmail.com>
2024-02-12 19:49:11 -0300

    Update fonts and add header component

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HeroSection from '../components/home/HeroSection';
+import Header from '../components/common/Header';
 
 function HomePage() {
   return (
     <div>
+      <Header />
       <HeroSection />
     </div>
   );
