1f6fa3ba6014df5a88747865b1a825f2cc68cb70
dsthiago <thiagosd.2.18@gmail.com>
2024-02-12 19:25:13 -0300

    Update styles and add HeroSection to HomePage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import AboutPage from './views/AboutPage';
 import ContactPage from './views/ContactPage';
+import "./assets/styles/globals.css";
 
 function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
