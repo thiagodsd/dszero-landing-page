520ade21244d97bfd235806fe320ab8c82b037ce
dsthiago <thiagosd.2.18@gmail.com>
2024-02-12 19:25:13 -0300

    Update styles and add HeroSection to HomePage

---
+++ src/components/home/HeroSection.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../../assets/styles/HeroSection.css";
+
+import img_hero_01 from "../../assets/images/hero_logo_01.png";
+import img_hero_02 from "../../assets/images/hero_logo_02.png";
+import img_hero_03 from "../../assets/images/hero_logo_03.png";
+
+function HeroSection() {
+    return (
+        <div>
+            <div className="hero-container">
+                <div className="hero-section-left">
+                    <h1>Reduza Desordem<br/>Minimize Esforços<br/>Atinja Objetivos</h1>
+                    <h2>Desbloqueie todo o potencial que os seus dados possuem</h2>
+                    <h3>Garimpamos resultados nos seus dados através de soluções estruturadas por  técnicas e ferramentas de ciência de dados, aprendizagem de máquina e desenvolvimento de software.</h3>
+                </div>
+                <div className="hero-section-right">
+                    <div className="figure">
+                        <img src={img_hero_01} alt="Hero Section Logo" />
+                        <img src={img_hero_02} alt="Hero Section Logo" />
+                        <img src={img_hero_03} alt="Hero Section Logo" />
+                    </div>
+                </div>
+            </div>
+            {/* Other content of the HeroSection can go here */}
+        </div>
+    );
+}
+
+export default HeroSection;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import HeroSection from '../components/home/HeroSection';
 
 function HomePage() {
-  return <div>Home Page Content</div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
 
 export default HomePage;
