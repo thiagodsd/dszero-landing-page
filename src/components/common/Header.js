8bacb3f13f7e4dc93a426ac0cf2e95da61e0ac81
dsthiago <thiagosd.2.18@gmail.com>
2024-02-12 19:49:11 -0300

    Update fonts and add header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../assets/styles/Header.css";
+
+function Header() {
+    return (
+        <div>
+            <div className="header-container">
+                <p className="char-delta">&#948;</p><p className="char-s">S</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
