7e2dc16199249dd4bcb8aa9f511024438826ae33
dsthiago <thiagosd.2.18@gmail.com>
2024-02-17 16:44:44 -0300

    Add PortfolioSection to HomePage and PortfolioItemPage to App

---
+++ src/components/portfolio/projects/clustering-project-metadata.js
@@ -0,0 +1,7 @@
+module.exports = {
+        id: 'clustering-project',
+        title: 'Clustering Project',
+        description: 'A detailed case study on data clustering',
+        tags: ['Data Science', 'Clustering', 'Machine Learning'],
+        featuredImage: 'path/to/clustering-project/image.jpg',
+};
---
+++ src/components/portfolio/projects/regression-project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import metadata from './clustering-project-metadata';
+
+const ClusteringProject = () => (
+    <div>
+        <h1>{metadata.title}</h1>
+        {/* Project content */}
+    </div>
+);
+
+export default ClusteringProject;
