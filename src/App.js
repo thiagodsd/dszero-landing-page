2e0ab2aec6999c903509aa56d049ac31f7b23c09
dsthiago <thiagosd.2.18@gmail.com>
2024-02-25 11:43:06 -0300

    Add AOS library and update global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./views/HomePage";
 import AboutPage from "./views/AboutPage";
 import ContactPage from "./views/ContactPage";
 import PortfolioItemPage from "./components/portfolio/PortfolioItemPage";
+
 import "./assets/styles/globals.css";
+import 'aos/dist/aos.css';
 
 function App() {
+    useEffect(() => {
+        AOS.init({
+            duration : 1200,
+            data_aos_offset : 300,
+            data_aos_easing : "ease-in-sine"
+        });
+    }, []);
     return (
         <div>
             <Routes>
