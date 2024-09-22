0b486c143e3456c149d0a3079acb27230c3eef3c
dsthiago <thiagosd.2.18@gmail.com>
2024-09-22 16:11:54 -0300

    Refactor project configuration files

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <secttion>
+            <div className="container">
+                <h1 className="text-3xl font-bold">
+                    Hello, world!
+                </h1>
+            </div>
+        </secttion>
+    );
+}
