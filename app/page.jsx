632185bd6723860b5f51c6c9372842779deeabf6
dsthiago <thiagosd.2.18@gmail.com>
2024-09-22 17:49:02 -0300

    add new nextjs files

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <secttion className="py-24">
+            <div className="container">
+                <h1 className="mt-4 text-3xl font-bold">
+                    Hello, world!
+                </h1>
+            </div>
+        </secttion>
+    );
+}
