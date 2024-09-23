06e862e70d682d588f2d69d4e5289c83ec2fd56c
dsthiago <thiagosd.2.18@gmail.com>
2024-09-23 15:08:22 -0300

    Refactor layout and add header and footer components

---
+++ app/components/layout/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavLink from '../ui/NavLink';
+import ContactButton from '../ui/ContactButton';
+
+const Header = () => (
+    <header className="bg-gray-500 text-white">
+        <nav className="container">
+            <ul className="flex gap-4 p-4">
+                <li>
+                    <NavLink href="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink href="/about">About</NavLink>
+                </li>
+                <ContactButton />
+            </ul>
+        </nav>
+    </header>
+);
+
+export default Header;
