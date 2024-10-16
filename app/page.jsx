93d53f0b1c200a22c92e1a589e75aeacd070283d
dsthiago <thiagosd.2.18@gmail.com>
2024-10-16 07:51:02 -0300

    Refactor layout and fix typo in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <secttion className="py-24">
+        <section className="py-24">
             <div className="container">
                 <h1 className="mt-4 text-3xl font-bold">
                     Hello, world!
                 </h1>
             </div>
-        </secttion>
+        </section>
     );
 }
